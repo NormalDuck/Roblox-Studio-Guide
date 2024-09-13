da1ef6a8f1dba80e70b3e29c969f31e0dc0ff52c
NormalDuck <1juhuang2@duck.com>
2024-09-12 21:07:00 -0700

    feat: test icon

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Start your adventure 🚀
+            🚀Start your adventure 🚀
           </Link>
         </div>
       </div>
